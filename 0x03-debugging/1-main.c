6c11e049cab0900aa38698e13b06bc4ce5b372ca
ghislain2022 <amegadjekomla560@gmail.com>
2022-09-14 08:46:32 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinit loop
+* Return: Ã0
+*/
+
+int main(void)
+{
+int i;
+printf("infinite loop incoding :(\n");
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("infinite loop avoided! \\o/\n");
+return (0);
+}

d499ca062d89d6596a38913338f5553a44c0532c
ghislain2022 <amegadjekomla560@gmail.com>
2022-09-14 08:32:29 -0700

    fixed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-void postitive_or_negative(int i);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
-#endif /* MAIN_H */
